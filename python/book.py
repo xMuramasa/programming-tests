07d8f86e00a5e82477c1a0c1728c23b6281bae40
Felipe Silva De Vidts <fsilva@mq.cl>
2023-12-04 18:03:23 -0300

    Add base book

---
+++ python/book.py
@@ -0,0 +1,23 @@
+class SQL:
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def list(self):
+        pass
+
+    def retrieve(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+
+class Book:
+    """Aquí implementar la clase"""
+
+
